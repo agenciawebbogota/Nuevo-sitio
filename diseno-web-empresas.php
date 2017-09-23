ea79a5c70dcc22d3b0358d3a16903d7375d86e93
PabloMart81 <pablomart81@gmail.com>
2017-09-23 18:37:52 -0500

    Sabado 23/09
    Diseño web empresas

---
+++ diseno-web-empresas.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html class="no-js" lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Diseño Web Empresas</title>
+    <meta name="description" content="Nexus is a modern & responsive HTML5 template.">
+    <meta name="viewport" content="width=device-width, initial-scale=0.85">
+    <meta name="HandheldFriendly" content="True">
+    <meta name="MobileOptimized" content="320">
+    <!-- Icons & favicons -->
+    <link rel="apple-touch-icon" href="archivos/images/apple-icon-touch.png">
+    <link rel="icon" href="archivos/images/favicon.png">
+    <link rel="stylesheet" type="text/css" href="archivos/font-awesome/css/font-awesome.min.css">
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="assets/styles/css/app.css">
+    <!-- Modernizr -->
+    <script src="assets/js/modernizr.min.js"></script>
+</head>
+<body class="further services">
+    <!-- SITE CONTENT -->
+    <div id="site-content">
+        <!-- Header -->
+<header class="app-header" id="app-header">
+    <div class="container">
+        <div class="header-wrapper">
+            <!-- Logo -->
+            <div class="logo" id="logo">
+                <!-- image logo -->
+                <a href="index.php" class="image-logo">
+                    <img src="assets/images/logo-agencia-web-bogota.svg" alt="Logo Agencia Web Bogota" title="Agencia Web Bogota" />
+                </a>
+            </div>
+            <!-- Main-Nav -->
+            <nav class="main-nav">
+                <ul>
+                    <li><a href="index.php">Inicio</a></li>
+                    <!-- Dropdown Nav -->
+                    <li>
+                        <a>Emprendedores</a>
+                        <ul>
+                            <li>
+                                <a href="diseno-web-emprendedores.php">Diseño Web</a>
+                            </li>
+                            <li>
+                                <a href="marketing-digital-emprendedores.php">Marketing Digital</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="active">
+                        <a>Empresas</a>
+                        <ul>
+                            <li>
+                                <a href="diseño-web-empresas.php">Diseño Web</a>
+                            </li>
+                            <li>
+                                <a href="index-single.html">Marketing Digital</a>
+                            </li>
+                            <li>
+                                <a href="index-single.html">Aplicativos Web</a>
+                            </li>
+                            <li>
+                                <a href="google-apps.php">Google App</a>
+                            </li>
+                            <li>
+                                <a href="index-single.html">Hosting</a>
+                            </li>
+                            <li>
+                                <a href="index-single.html">Posicionamiento</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="index-single.html">Cursos</a></li>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="contact.html">Contacto</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+        <!-- Sub-Header -->
+
+        <section class="hero sub-header">
+    <div class="container inactive">
+        <div class="sh-title-wrapper">
+            <h1>Diseño Web Empresas</h1>
+            <p>Diseño Web Profesional Corporativo enfocado en el cumplimiento de objetivos empresariales</p>
+        </div>
+    </div>
+</section>
+        <!-- Breadcrumb -->
+
+        <nav class="breadcrumb">
+    <div class="container">
+        <ul>
+            <li class="home"><a href="index.php"><i class="fa fa-home"></i></a></li>
+            <li><a href="index.php">Inicio</a></li>
+        </ul>
+    </div>
+</nav>
+        <!-- Main Content -->
+<section class="section secondary more-info" id="s-more-info">
+<div class="container">
+<div class="row">
+    <div class="span-6 info-video">
+        <div class="auto-resizable-iframe">
+            <div>
+              <img src="assets/images/responsive_websites.png" alt="Página web cargada en diferentes dispositivos" title="Diseño Web Optimizado">
+            </div>
+        </div>
+    </div>
+    <div class="span-6 more-info-meta">
+        <div class="section-title">
+            <h2>Agencia Web Bogotá</h2>
+            <h3><i>Diseño Web Empresas</i></h3>
+        </div>
+        <p align="justify">Contamos con los conocimientos, las estrategias, los recursos y la experiencia necesaria para darle un gran impulso a su empresa en el mundo virtual.<br/>
+          Más que desarrollar simples páginas de Internet, creamos portales web altamente efectivos, funcionales y con gran capacidad comunicativa.</p>
+        <ul class="bullet-list min-bp2">
+            <li>Implementamos Neuromarketing en todos nuestros diseños</li>
+            <li>Trabajamos con Temas y Plugins Premium de WordPress</li>
+            <li>Diseño único y sin plantillas en HTML5 + CSS3 + JavaScript</li>
+            <li>Incorporamos aplicativos web especiales según su necesidad</li>
+            <li>Le entregamos su Sitio Web Posicionado en Google</li>
+        </ul>
+        <p class="button-set">
+            <a href="#s-contacto"
+                class="button round brand-1">Solicite su cotización</a>
+        </p>
+    </div>
+</div>
+</div>
+</section>
+<section class="section secondary skills inactive">
+        <div class="container">
+        <div class="row block-columns">
+            <div class="span-6 skills-meta">
+                <p class="mision" style="font-size:25px">SU SITIO WEB DESDE CERO SIN PLANTILLAS<br />
+                <b>Diseños, estructuras y contenidos únicos</b></p>
+                <ul class="bullet-list min-bp2">
+                    <li>Desarrollamos en HTML5 + CSS3 + JAVASCRIPT + PHP + JQUERY</li>
+                    <li>Código limpio, liviano y sin errores</li>
+                    <li>Diseños creativos, innovadores y con alto impacto publicitario</li>
+                    <li>Imágenes y gráficos con licencia premium</li>
+                    <li>Desarrollamos los aplicativos web que requiera su empresa</li>
+                    <li>Los Sitios creados en HTML5 son más amigables con el SEO</li>
+                    <li>No son administrables a menos que tenga conocimientos en programación</li>
+                </ul>
+            </div>
+            <div class="team-pic">
+                    <div class="mask"></div>
+                    <img src="assets/images/servicios_digitales_empresa.png" alt="Infografia servicios para empresa" title="Servicios Digitales para empresa" />
+                </div>
+        </div>
+    </div>
+</section>
+<section class="section secondary skills inactive">
+        <div class="container">
+        <div class="row block-columns">
+          <div class="team-pic">
+                  <div class="mask"></div>
+                  <img src="assets/images/servicios_digitales_empresa.png" alt="Infografia servicios para empresa" title="Servicios Digitales para empresa" />
+          </div>
+            <div class="span-6 skills-meta">
+                <p class="mision" style="font-size:25px">SU SITIO WEB EN WORDPRESS<br />
+                <b>Con Temas y Plugins Premium</b></p>
+                <ul class="bullet-list min-bp2">
+                    <li>El sistema más usado en páginas web en el mundo</li>
+                    <li>Ideal para manejo de contenidos tipo blog</li>
+                    <li>Compatible con tiendas online e - commerce</li>
+                    <li>Completamente administrable</li>
+                    <li>Sitios livianos con excelente velocidad de carga </li>
+                    <li>Autoajustables a dispositivos móviles</li>
+                    <li>Miles de plugins facilmente instalables</li>
+                    <li>Plantillas 100% personalizables</li>
+                    <li>Plugins premium de posicionamiento en buscadores</li>
+                    <li>Efectos y animaciones alucinantes</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section primary latest-works" id="portfolio">
+<div class="container">
+<header class="sep active">
+    <div class="section-title">
+        <h3>Algunos de nuestros más recientes trabajos</h3>
+    </div>
+    <p>Consolidar la presencia de las empresas en Internet ha sido nuestra motivación desde nuestros inicios</p>
+</header>
+<div class="portfolio-items">
+    <div class="portfolio-item">
+        <div class="controls">
+            <a href="single-project.html" class="icon-round-border">
+                <i class="fa fa-link"></i>
+            </a>
+            <a href="http://placehold.it/800x543" class="icon-round-border icon-view">
+                <i class="fa fa-search"></i>
+            </a>
+        </div>
+        <h4><a href="single-project.html">Viaggi Group</a></h4>
+        <p>programas educativos en el exterior</p>
+        <img src="http://placehold.it/800x543" alt="" />
+    </div>
+    <div class="portfolio-item">
+        <div class="controls">
+            <a href="single-project.html" class="icon-round-border">
+                <i class="fa fa-link"></i>
+            </a>
+            <a href="http://placehold.it/800x543" class="icon-round-border icon-view">
+                <i class="fa fa-search"></i>
+            </a>
+        </div>
+        <h4><a href="single-project.html">Servinproh</a></h4>
+        <p>Administración de propiedad horizontal</p>
+        <img src="http://placehold.it/800x543" alt="" />
+    </div>
+    <div class="portfolio-item">
+        <div class="controls">
+            <a href="single-project.html" class="icon-round-border">
+                <i class="fa fa-link"></i>
+            </a>
+            <a href="http://placehold.it/800x543" class="icon-round-border icon-view">
+                <i class="fa fa-search"></i>
+            </a>
+        </div>
+        <h4><a href="single-project.html">Quiroz Encuadernación</a></h4>
+        <p>Empaste de libros contables</p>
+        <img src="http://placehold.it/800x543" alt="" />
+    </div>
+    <div class="portfolio-item max-bp2">
+        <div class="controls">
+            <a href="single-project.html" class="icon-round-border">
+                <i class="fa fa-link"></i>
+            </a>
+            <a href="http://placehold.it/800x543" class="icon-round-border icon-view">
+                <i class="fa fa-search"></i>
+            </a>
+        </div>
+        <h4><a href="single-project.html">Medefiende</a></h4>
+        <p>Abogados especializados en defensa médica</p>
+        <img src="http://placehold.it/800x543" alt="" />
+    </div>
+</div>
+</div>
+</section>
+<section class="section banner alt">
+<div class="container">
+<div class="content">
+    <h4>Pregúntenos por los <b>beneficios adicionales</b> que le ofrecemos al contratar su <i>Sitio web</i> con nosotros</h4>
+</div>
+<div class="meta">
+    <a href="#" class="button brand-1 round">Solicitar Cotización</a>
+</div>
+</div>
+</section>
+<?php
+    require_once 'includes/footer.php';
+
+ ?>
--- marketing-digital-emprendedores.php
+++ marketing-digital-emprendedores.php
@@ -154,7 +154,7 @@
 <div class="container">
 <header class="sep active">
     <div class="section-title">
-        <h2>¿Que te <i>Ofrecemos?</i></h2>
+        <h2>¿Que mas te<i>Ofrecemos?</i></h2>
         <h3>Esto no termina aquí...</h3>
     </div>
     <p>Nuestros planes de <a>Marketing Digital para emprendedores</a> van más allá de estrategias integrales sin rumbo. Agencia Web Bogotá diseñará
@@ -164,141 +164,48 @@
 <div class="row feature-blocks inactive">
     <div class="span-3 feature-block-wrapper">
         <div class="feature-block">
-            <i class="fa fa-magic fa-2x" style="color:#fff"></i>
+            <i class="fa fa-comments fa-2x" style="color:#fff"></i>
             <h4>Community Manager</h4>
             <h5>Redes Sociales</h5>
             <p>Despreocupate de posicionar tu marca y difundir tus contenidos en redes sociales. Nosotros nos encargaremos de tus anuncios y publicaciones.
-              Todo de manera profesional, acertada y efectiva.</p>
+              Todo de manera profesional, acertada y efectiva</p>
         </div>
     </div>
     <div class="span-3 feature-block-wrapper">
         <div class="feature-block">
-            <i class="fa fa-magic fa-2x" style="color:#fff"></i>
+            <i class="fa fa-at  fa-2x" style="color:#fff"></i>
             <h4>Email Marketing</h4>
             <h5>Envío de correos masivos</h5>
             <p>Aunque este es un canal relativamente antiguo comparado con las nuevas estrategias de Marketing Digital, podemos asegurarte que sacaremos
-            el mejor provecho de tus listas de correo.</p>
+            el mejor provecho de tus listas de correo electrónico</p>
         </div>
     </div>
     <div class="span-3 feature-block-wrapper">
         <div class="feature-block">
-            <i class="fa fa-magic fa-2x" style="color:#fff"></i>
-            <h4>Valid HTML5</h4>
-            <h5>Donec finibus fringer</h5>
-            <p>Suspendisse tempus a nisl maximus, ultrices est ut blandit nislr, elit in lobortis mattis.</p>
+            <i class="fa fa-desktop fa-2x" style="color:#fff"></i>
+            <h4>Landing Pages</h4>
+            <h5>Páginas de aterrizaje</h5>
+            <p>Diseñamos y programamos páginas de captura y aterrizaje para tus campañas en Internet, destacando tus ofertas, servicios o artículos.
+            Enlazadas a tu correo electrónico, base de datos o listas de mailchimp</p>
         </div>
     </div>
     <div class="span-3 feature-block-wrapper">
         <div class="feature-block">
-            <i class="fa fa-magic fa-2x" style="color:#fff"></i>
-            <h4>Free Support</h4>
-            <h5>Donec finibus fringer</h5>
-            <p>Suspendisse tempus a nisl maximus, ultrices est ut blandit nislr, elit in lobortis mattis.</p>
+            <i class="fa fa-line-chart fa-2x" style="color:#fff"></i>
+            <h4>Neuromarketing</h4>
+            <h5>Nuestra especialidad</h5>
+            <p>Aplicamos conceptos de neuromarketing en todas nuestras estrategias de marketing digital. Esto nos permite garantizarte resultados
+            reales en poco tiempo y sin invertir grandes sumas de dinero</p>
         </div>
     </div>
 </div>
 </div>
 </section>
-
-<section class="section secondary skills inactive">
-        <div class="container">
-        <div class="row block-columns">
-            <div class="span-5 skills-meta">
-                <div class="section-title">
-                    <h3 style="line-height:110%">Nosotros también somos emprendedores</h3>
-                </div>
-                <p class="mision" style="font-size:25px">Conocemos y entendemos tus necesidades</p>
-                <p class="mision2">Sabemos que en ocasiones es algo difícil crear empresa y contar con el apoyo necesario para iniciar el camino<br/>
-                  hacia tu sueño empresarial. Por eso nosotros
-                nos convertiremos en tus principales aliados en busca de lograr consolidar tu proyecto en Internet.
-                Tenemos amplia experiencia en Diseño Web Emprendedores, respaldada por conocimientos en creación de empresas y marketing digital
-            especializado en pymes.</p>
-            <p class="mision" style="font-size:20px">¡Deja tu proyecto en las mejores manos!</p>
-            </div>
-            <div class="team-pic">
-                    <img src="assets/images/servicios_digitales_empresa.png" alt="Infografia servicios para empresa" title="Servicios Digitales para empresa" />
-                </div>
-        </div>
-    </div>
-</section>
-<section class="section primary pricing inactive" id="s-pricing">
-    <div class="container">
-        <header class="sep active">
-            <div class="section-title">
-                <h2>Nuestros <i>Planes</i></h2>
-                <h3>Diseño Web a tu medida</h3>
-            </div>
-            <p>En Agencia Web Bogotá te ofrecemos precios especiales y facilidades de pago para el desarrollo de tu proyecto de emprendimiento empresarial</p>
-        </header>
-        <div class="row price-charts">
-          <div class="span-4 price-chart-container">
-              <div class="price-chart">
-                  <div class="ribbon ribbon-large">
-                      <div class="banner">
-                          <div class="text">Mejor Oferta</div>
-                      </div>
-                  </div>
-                  <h4>Sitio Basico</h4>
-                  <h5>El ideal para comenzar</h5>
-                  <div class="price">
-                      <span>$79</span>.000 + IVA/mes
-                  </div>
-                  <ul>
-                      <li><span><i class="fa fa-desktop"></i> Diseño Personalizado</span></li>
-                      <li><span><i class="fa fa-search"></i> Posicionamiento SEO</span></li>
-                      <li><span><i class="fa fa-mobile"></i> Versión Móviles</span></li>
-                      <li><span><i class="fa fa-support"></i> Hosting y Dominio Incluído</span></li>
-                  </ul>
-                  <div class="buy-now">
-                      <a href="#" class="button brand-1 full-width">¡Contrátalo Ahora!</a>
-                  </div>
-              </div>
-          </div>
-            <div class="span-4 price-chart-container">
-                <div class="price-chart">
-                    <h4>Sitio Corporativo</h4>
-                    <h5>Ideal para PYMES</h5>
-                    <div class="price">
-                        <small>Desde </small><span>$650</span>.000+IVA
-                    </div>
-                    <ul>
-                        <li><span><i class="fa fa-html5"></i> WordPress o HTML5</span></li>
-                        <li><span><i class="fa fa-envelope"></i> Correos Corporativos</span></li>
-                        <li><span><i class="fa fa-database"></i> Bases de datos</span></li>
-                        <li><span><i class="fa fa-code"></i> Contenido Administrable</span></li>
-                    </ul>
-                    <div class="buy-now">
-                        <a href="#" class="button brand-1 full-width">¡Contrátalo Ahora!</a>
-                    </div>
-                </div>
-            </div>
-            <div class="span-4 price-chart-container">
-                <div class="price-chart">
-                    <h4>Tienda Online</h4>
-                    <h5>E - Commerce</h5>
-                    <div class="price">
-                        <small>Desde </small><span>$900</span>.000+IVA
-                    </div>
-                    <ul>
-                        <li><span><i class="fa fa-wordpress"></i> Woocomerce o Prestashop</span></li>
-                        <li><span><i class="fa fa-shopping-cart"></i> Carrito de Compra</span></li>
-                        <li><span><i class="fa fa-credit-card"></i> Pasarela de Pagos Online</span></li>
-                        <li><span><i class="fa fa-lock"></i> Seguridad SSL</span></li>
-                    </ul>
-                    <div class="buy-now">
-                        <a href="#" class="button brand-1 full-width">¡Contrátalo Ahora!</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
 
 <section class="section banner2 alt">
     <div class="container">
         <div class="content">
-            <h4>Además <i>Agencia Web Bogotá</i> te cobrará la primer mensualidad cuando tu Sitio Web esté <b>100% Terminado y Online</b> no te exigimos <i>anticipos</i></h4>
+            <h4>en <i>Agencia Web Bogotá</i> somos especialistas en<br/><b>Marketing Digital para Emprendedores</b></h4>
             <p>Contáctanos para más información <a href="#s-contacto">sobre nuestros planes y servicios</a></p>
         </div>
     </div>
