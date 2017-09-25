ad624ba4c695dacff24e451c2adf3cd63c065944
PabloMart81 <pablomart81@gmail.com>
2017-09-24 19:29:12 -0500

    Domingo 24/09
    Marketing digital empresas

---
+++ marketing-digital-empresas.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html class="no-js" lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Marketing Digital Emprendedores</title>
+    <meta name="description" content="Nexus is a modern & responsive HTML5 template.">
+    <meta name="viewport" content="width=device-width, initial-scale=0.85">
+    <meta name="HandheldFriendly" content="True">
+    <meta name="MobileOptimized" content="320">
+    <!-- Icons & favicons -->
+    <link rel="apple-touch-icon" href="archivos/images/apple-icon-touch.png">
+    <link rel="icon" href="archivos/images/favicon.png">
+    <link rel="stylesheet" type="text/css" href="archivos/font-awesome/css/font-awesome.min.css">
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="assets/styles/css/app.css">
+    <!-- Modernizr -->
+    <script src="assets/js/modernizr.min.js"></script>
+</head>
+<body class="further services">
+    <!-- SITE CONTENT -->
+    <div id="site-content">
+        <!-- Header -->
+<header class="app-header" id="app-header">
+    <div class="container">
+        <div class="header-wrapper">
+            <!-- Logo -->
+            <div class="logo" id="logo">
+                <!-- image logo -->
+                <a href="index.php" class="image-logo">
+                    <img src="assets/images/logo-agencia-web-bogota.svg" alt="Logo Agencia Web Bogota" title="Agencia Web Bogota" />
+                </a>
+            </div>
+            <!-- Main-Nav -->
+            <nav class="main-nav">
+                <ul>
+                    <li><a href="index.php">Inicio</a></li>
+                    <!-- Dropdown Nav -->
+                    <li>
+                        <a>Emprendedores</a>
+                        <ul>
+                            <li>
+                                <a href="diseno-web-emprendedores.php">Diseño Web</a>
+                            </li>
+                            <li>
+                                <a href="marketing-digital-emprendedores.php">Marketing Digital</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="active">
+                        <a>Empresas</a>
+                        <ul>
+                            <li>
+                                <a href="diseno-web-empresas.php">Diseño Web</a>
+                            </li>
+                            <li>
+                                <a href="marketing-digital-empresas.php">Marketing Digital</a>
+                            </li>
+                            <li>
+                                <a href="index-single.html">Aplicativos Web</a>
+                            </li>
+                            <li>
+                                <a href="google-apps.php">Google App</a>
+                            </li>
+                            <li>
+                                <a href="index-single.html">Hosting</a>
+                            </li>
+                            <li>
+                                <a href="index-single.html">Posicionamiento</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="index-single.html">Cursos</a></li>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="contact.html">Contacto</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+        <!-- Sub-Header -->
+
+        <section class="hero sub-header">
+    <div class="container inactive">
+        <div class="sh-title-wrapper">
+            <h1>Marketing Digital para Empresas</h1>
+            <p>El marketing digital es una combinación de arte, teconología, causa, efecto, magia y lógica</p>
+        </div>
+    </div>
+</section>
+        <!-- Main Content -->
+
+        <section class="section secondary skills inactive">
+                <div class="container">
+                <div class="row block-columns">
+                    <div class="span-6 skills-meta">
+                        <p class="mision" style="font-size:25px">ESTRATEGIAS DE MARKETING DIGITAL 3.0<br/>ENRIQUECIDAS CON NEUROMARKETING</p>
+                        <ul class="bullet-list min-bp2">
+                            <li>Anuncios publicitarios más efectivos</li>
+                            <li>Conceptos de neuroventas probados</li>
+                            <li>Estrategias claras, definidas y con resultados reales</li>
+                            <li>Aprovechamos todo el potencial de Internet a su favor</li>
+                            <li>Campañas de publicidad digital 300% más efectivas</li>
+                            <li>Redes sociales con más seguidores y mayor alcance</li>
+                            <li>Contenidos de calidad para su sitio web y blog</li>
+                            <li>Experiencia, tecnología y creatividad a su servicio</li>
+                            <li>Planes mensuales económicos</li>
+                        </ul>
+                    </div>
+                    <div class="team-pic">
+                            <div class="mask"></div>
+                            <img src="assets/images/servicios_digitales_empresa.png" alt="Infografia servicios para empresa" title="Servicios Digitales para empresa" />
+                        </div>
+                </div>
+            </div>
+        </section>
+<section class="section primary what-we-offer inactive" id="s-what-we-offer">
+<div class="container">
+<div class="row feature-blocks inactive">
+    <div class="span-3 feature-block-wrapper">
+        <div class="feature-block">
+            <i class="fa fa-comments fa-2x" style="color:#fff"></i>
+            <h4>Inbound Marketing</h4>
+            <h5>Marketing de Contenidos</h5>
+            <p>Creamos contenidos de calidad para su blog o página web, que le permitirán posicionar su marca, crear una
+            gran lista de prospectos, fidelizar a sus clientes y convertir a su empresa en una autoridad en su nicho</p>
+        </div>
+    </div>
+    <div class="span-3 feature-block-wrapper">
+        <div class="feature-block">
+            <i class="fa fa-at  fa-2x" style="color:#fff"></i>
+            <h4>Community Manager</h4>
+            <h5>Redes Sociales</h5>
+            <p>Gestionamos eficazmente todas sus redes sociales por medio de la herramienta <b>HOOTSUITE</b>. Esto permite automatizar
+            la gestión de contenido en redes sociales, asegurarse de la calidad de los enlaces y aumentar el número de likes</p>
+        </div>
+    </div>
+    <div class="span-3 feature-block-wrapper">
+        <div class="feature-block">
+            <i class="fa fa-desktop fa-2x" style="color:#fff"></i>
+            <h4>SEM Y SOCIAL MEDIA</h4>
+            <h5>Campañas de pago</h5>
+            <p>Diseñamos sus campañas publicitarias de pago en Google Adwords, Facebook Ads, Twitter Ads y demás redes sociales. Contamos con un
+            equipo experto en la cosntrucción de campañas y anuncios en Internet que le asesorará.</p>
+        </div>
+    </div>
+    <div class="span-3 feature-block-wrapper">
+        <div class="feature-block">
+            <i class="fa fa-line-chart fa-2x" style="color:#fff"></i>
+            <h4>Email Marketing</h4>
+            <h5>Envío de Correos masivos</h5>
+            <p>Aprovechamos al máximo su lista de correos electrónicos para el envío de información relacionada con su empresa. Usamos el
+              eficiente sistema de Autorespondedor <b>GET RESPONSE</b> que le brinda seguridad y efectividad en sus campañas de emailing</p>
+        </div>
+    </div>
+</div>
+</div>
+</section>
+<section class="section secondary more-info" id="s-more-info">
+<div class="container">
+<div class="row">
+    <div class="span-12 more-info-meta">
+        <div class="section-title">
+            <h2 style="font-size:30px !important;">¿Cómo aplicamos el Neuromarketing al Marketing Digital?</h2>
+            <h3><a>¿Qué es el Neuromarketing?</a></h3>
+        </div>
+        <p align="justify">Para aplicar técnicas de neuroventas en sus campañas publicitarias no es necesario invertir grandes sumas de dinero en equipos
+        especializados. Los estudios realizados durante años en el campo de la neurociencia nos permiten hoy en día conocer e implementar esos conceptos
+      en cualquier tipo de negocio.</p>
+      <p align="justify">No que todas las personas reaccionen de igual manera ante los estímulos de la publicidad, pero si existen unas tendencias demasiado
+      marcadas en el comportamiento del ser humano y la forma como el cerebro percibe la información.</p>
+    </div>
+</div>
+</div>
+</section>
+<section class="section secondary more-info" id="s-more-info" style="padding-top:0px;">
+<div class="container">
+<div class="row">
+    <div class="span-6 more-info-meta">
+        <p align="justify">Ahora, en el Marketing Digital existen diferentes espectos como anuncios, contenidos, imágenes, títulos, colores,
+         entre otros, que son optimizables por medio de las técnicas de neuromarketing. <br/>Un ejemplo de ello es el <b>COPYWRITING</b>, que trata del uso
+        de contenidos que despiertan emociones en el lector.<br/>Otro ejemplo práctico es la <a>publicidad diferenciada para hombres y mujeres</a>; y otro caso
+       es el uso de la psicología del color en los anuncios, banners y demás material publicitario.</p>
+       <p align="justify">Así que <a>el neuromarketing es la evolución del marketing convencional</a> y lo estamos aplicando cada vez con más fuerza y con mejores resultados
+        en todas nuestras campañas de Marketing Digital.<br/><br/></p>
+
+    </div>
+    <div class="span-6 info-video">
+        <div class="auto-resizable-iframe">
+            <div>
+              <img src="assets/images/responsive_websites.png" alt="Página web cargada en diferentes dispositivos" title="Diseño Web Optimizado">
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</section>
+
+<section class="section banner alt">
+<div class="container">
+<div class="content">
+    <h4>Tenemos la estrategia perfecta de Marketing Digital para su compañía</h4>
+
+</div>
+<div class="meta">
+    <a href="#" class="button brand-1 round">Contáctenos ¡Ahora!</a>
+</div>
+</div>
+</section>
+<?php
+    require_once 'includes/footer.php';
+
+ ?>
