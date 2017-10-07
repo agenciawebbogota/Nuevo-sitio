852100402d14478fa340ccf378c50570162d3754
whary11 <whary11@gmail.com>
2017-10-07 09:57:38 -0500

    Primeros cambios en las rutas de la página aplicativos web

---
+++ aplicativos-web.php
@@ -0,0 +1,381 @@
+<!DOCTYPE html>
+<html class="no-js" lang="es">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta http-equiv="Content-Language" content="es">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <meta name="HandheldFriendly" content="True">
+  <meta name="MobileOptimized" content="320">
+  <meta name="Robots" content="index, follow">
+  <title>Diseño Web Emprendedores | Agencia Web Bogotá</title>
+  <meta name="description" content="Diseño Web Emprendedores desde $79.000 + IVA por mes. Con Hosting, Dominio, Posicionamiento y versión móviles">
+  <meta name="keywords" content="agencia web bogota, diseño web emprendedores, diseño web bogota, diseño de paginas web Bogota, páginas web corporativas,  diseño de paginas web, diseño paginas web economicas, diseño web optimizado, neuromarketing">
+  <meta name="author" content="Agencia Web Bogotá">
+  <meta name="email" content="agenciawebbogota@gmail.com">
+  <meta name="Rating" content="General">
+  <meta name="msvalidate.01" content="607F58A810BE9AAB1051493A749C22E4" />
+  <meta name="Distribution" content="Local">
+  <link rel="alternate" hreflang="es" href="https://agenciawebbogota.com/" />
+  <meta property="og:type" content="website" />
+  <meta property="og:site_name " content="Agencia Web Bogotá" />
+  <meta property="og:title" content="Diseño Web Emprendedores | Agencia Web Bogotá" />
+  <meta property="og:url" content="https://agenciawebbogota.com" />
+  <meta property="og:image" content="https://agenciawebbogota.com/anuncios/agencia_web_bogota.png" />
+  <meta property="og:description" content="Diseño Web Emprendedores desde $79.000 + IVA por mes. Con Hosting, Dominio, Posicionamiento y versión móviles" />
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="@AgenciaBogota">
+  <meta name="twitter:creator" content="@AgenciaBogota">
+  <meta name="twitter:image" content="https://agenciawebbogota.com/anuncios/twitter-card.jpg">
+  <meta name="twitter:image:alt" content="Agencia Web Bogotá | Diseño Web Emprendedores">
+  <meta name="twitter:title" content="Diseño Web Emprendedores">
+  <meta name="twitter:description" content="Diseño Web Emprendedores desde $79.000 + IVA por mes. Con Hosting, Dominio, Posicionamiento y versión móviles">
+    <!-- Icons & favicons -->
+    <link rel="apple-touch-icon" href="archivos/images/apple-icon-touch.png">
+    <link rel="icon" href="archivos/images/favicon.png">
+    <link rel="stylesheet" type="text/css" href="archivos/font-awesome/css/font-awesome.min.css">
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="assets/styles/css/app.css">
+    <style type="text/css">
+        .hero {
+            background-image: url("assets/images/diseno-emprendedores.jpg");
+        }
+        .section.secondary {
+          background-image: url("assets/images/emprendedores-background.jpg");
+        }
+    </style>
+    <!-- Modernizr -->
+    <script src="assets/js/modernizr.min.js"></script>
+</head>
+<body class="further services">
+    <!-- SITE CONTENT -->
+    <div id="site-content">
+        <!-- Header -->
+<header class="app-header" id="app-header">
+    <div class="container">
+        <div class="header-wrapper">
+            <!-- Logo -->
+            <div class="logo" id="logo">
+                <!-- image logo -->
+                <a href="index.php" class="image-logo">
+                    <img src="assets/images/logo-agencia-web-bogota.svg" alt="Logo Agencia Web Bogota" title="Agencia Web Bogota" />
+                </a>
+            </div>
+            <!-- Main-Nav -->
+            <nav class="main-nav">
+                <ul>
+                    <li><a href="index.php">Inicio</a></li>
+                    <!-- Dropdown Nav -->
+                    <li class="active">
+                        <a>Emprendedores</a>
+                        <ul>
+                            <li>
+                                <a href="diseno-web-emprendedores.php">Diseño Web</a>
+                            </li>
+                            <li>
+                                <a href="marketing-digital-emprendedores.php">Marketing Digital</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a>Empresas</a>
+                        <ul>
+                            <li>
+                                <a href="diseno-web-empresas.php">Diseño Web</a>
+                            </li>
+                            <li>
+                                <a href="marketing-digital-empresas.php">Marketing Digital</a>
+                            </li>
+                            <li>
+                                <a href="aplicativos-web.php">Aplicativos Web</a>
+                            </li>
+                            <li>
+                                <a href="google-apps.php">Google App</a>
+                            </li>
+                            <li>
+                                <a href="posicionamiento-web.php">Posicionamiento</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li style="cursor:pointer;"><a onclick='location.href ="http://academiawebbogota.com/"'>Cursos</a></li>
+                    <li style="cursor:pointer;"><a onclick='location.href ="https://agenciawebbogota.com/blog/"'>Blog</a></li>
+                    <li><a href="contacto.php">Contacto</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+        <!-- Sub-Header -->
+
+        <section class="hero sub-header">
+    <div class="container inactive">
+        <div class="sh-title-wrapper">
+            <h1>Diseño Web para Emprendedores</h1>
+            <p>Nuestra misión es ayudarte a crear y consolidar tu proyecto empresarial</p>
+        </div>
+    </div>
+</section>
+        <!-- Breadcrumb -->
+
+        <nav class="breadcrumb">
+    <div class="container">
+        <ul>
+            <li class="home"><a href="index.php"><i class="fa fa-home"></i></a></li>
+            <li><a href="index.php">Inicio</a></li>
+        </ul>
+    </div>
+</nav>
+        <!-- Main Content -->
+
+        <section class="section services primary section-map" id="services">
+    <div class="container">
+        <header class="sep active">
+            <div class="section-title">
+                <h2><i>La mejor opción</i> para tu <strong>Emprendimiento</strong></h2>
+            </div>
+            <p class="sub-text">En Agencia Web Bogotá conocemos y entendemos las necesidades de los emprendedores. Por eso hemos ideado
+            un paquete de diseño que va más allá de la simple construcción de una página web.</p>
+        </header>
+        <div class="row inactive">
+            <div class="span-4 widget service">
+                <div class="widget-content">
+                    <header>
+                        <img src="assets/images/web-design.png" alt="">
+                        <div class="title">
+                            <h4>Diseño Profesional</h4>
+                            <h5>Optimizado con Neuromarketing</h5>
+                        </div>
+                    </header>
+                    <p>Aplicamos conceptos probados de neuromarketing a todos nuestros diseños, lo que te garantiza un impacto positivo entre tus
+                    visitantes y una mayor recordación de tu marca.</p>
+                </div>
+            </div>
+            <div class="span-4 widget service">
+                <div class="widget-content">
+                    <header>
+                        <img src="assets/images/multiplataforma.png" alt="">
+                        <div class="title">
+                            <h4>Sitios Multiplataforma</h4>
+                            <h5>No todos son iguales</h5>
+                        </div>
+                    </header>
+                    <p>No construímos todos los sitios web con la misma técnica, conocemos los sistemas y como se ajustan a cada necesidad. Trabajamos con
+                      WordPress, Joomla, Prestashop, HTML5 + CSS3 + JavaScript</p>
+                </div>
+            </div>
+            <div class="span-4 widget service">
+                <div class="widget-content">
+                    <header>
+                        <img src="assets/images/imagenes-diseño-web.png" alt="">
+                        <div class="title">
+                            <h4>Imágenes Unicas</h4>
+                            <h5>Solo las verás en tu página</h5>
+                        </div>
+                    </header>
+                    <p>Contamos con suscripciones Premium a bancos de imágenes y material gráfico exclusivo que usaremos solo una vez. Así garantizamos que el contenido visual
+                    de tu sitio web sea único e irrepetible.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row inactive">
+            <div class="span-4 widget service">
+                <div class="widget-content">
+                    <header>
+                        <img src="assets/images/seo.png" alt="">
+                        <div class="title">
+                            <h4>Optimización SEO</h4>
+                            <h5>Posicionamiento en buscadores</h5>
+                        </div>
+                    </header>
+                    <p>Creamos tu sitio web lo mejor optimizado posible para que logre la mejor posición natural. Así cuando reducirás costos considerablemente si decides contratar
+                    un servicio profesional de posicionamiento.</p>
+                </div>
+            </div>
+            <div class="span-4 widget service">
+                <div class="widget-content">
+                    <header>
+                        <img src="assets/images/hosting.png" alt="">
+                        <div class="title">
+                            <h4>Hosting y Dominio</h4>
+                            <h5>Alojamiento web</h5>
+                        </div>
+                    </header>
+                    <p>Te incluímos el servicio de alojamiento web + dominio durante el primer año, en servidores seguros, rápidos y confiables. Tendrás además cuentas mail
+                    corporativas ilimitadas con el nombre de tu dominio.</p>
+                </div>
+            </div>
+            <div class="span-4 widget service">
+                <div class="widget-content">
+                    <header>
+                        <img src="assets/images/moviles.png" alt="">
+                        <div class="title">
+                            <h4>Versión Móviles</h4>
+                            <h5>Diseño Web Responsive</h5>
+                        </div>
+                    </header>
+                    <p>Todos nuestros diseños se adaptan perfectamente a cualquier dispositivo móvil, sin pérdida de contenido ni calidad en los gráficos e imágenes. Si requieres
+                    además te incluímos la versión AMP a muy bajo costo.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section primary latest-works" id="portfolio">
+<div class="container">
+<header class="sep active">
+    <div class="section-title">
+        <h3>Algunos de nuestros más recientes trabajos</h3>
+    </div>
+    <p>Somos especialistas en Diseño Web Emprendedores, cada semana ayudamos a forjar nuevos proyectos de emprendimiento empresarial con excelentes resultados.<br/>
+    Nuestro principal valor, el sentido de pertenencia por las nacientes empresas</p>
+</header>
+<div class="portfolio-items">
+    <div class="portfolio-item">
+        <div class="controls">
+            <a href="https://viaggigroup.co/" target="_blank" class="icon-round-border">
+                <i class="fa fa-link"></i>
+            </a>
+        </div>
+        <h4><a href="https://viaggigroup.co/" target="_blank">Viaggi Group</a></h4>
+        <p>programas educativos en el exterior</p>
+        <img src="assets/images/viaggi-group-pagina-web.jpg" alt="Viaggi Group" title="Viaggi Group" />
+    </div>
+    <div class="portfolio-item">
+        <div class="controls">
+            <a href="http://servinproh.com/" target="_blank" class="icon-round-border">
+                <i class="fa fa-link"></i>
+            </a>
+        </div>
+        <h4><a href="http://servinproh.com/" target="_blank">Servinproh</a></h4>
+        <p>Administración de propiedad horizontal</p>
+        <img src="assets/images/servinproh-pagina-web.jpg" alt="Servinproh" title="Servinproh" />
+    </div>
+    <div class="portfolio-item">
+        <div class="controls">
+            <a href="http://quirozencuadernacion.com/" target="_blank" class="icon-round-border">
+                <i class="fa fa-link"></i>
+            </a>
+        </div>
+        <h4><a href="http://quirozencuadernacion.com/" target="_blank">Quiroz Encuadernación</a></h4>
+        <p>Empaste de libros contables</p>
+        <img src="assets/images/quiroz-ecuadernacion-pagina.jpg" alt="" />
+    </div>
+    <div class="portfolio-item max-bp2">
+        <div class="controls">
+            <a href="http://www.medefiende.com/principal/" target="_blank" class="icon-round-border">
+                <i class="fa fa-link"></i>
+            </a>
+        </div>
+        <h4><a href="http://www.medefiende.com/principal/" target="_blank">Medefiende</a></h4>
+        <p>Abogados especializados en defensa médica</p>
+        <img src="assets/images/medefiende-pagina-web.jpg" alt="" />
+    </div>
+</div>
+</div>
+</section>
+<section class="section secondary skills inactive" style="height:auto;padding-top:60px;padding-bottom:60px;">
+        <div class="container">
+        <div class="row block-columns">
+            <div class="span-12 skills-meta">
+                <div class="section-title">
+                    <h3 style="font-weight:300;line-height:110%;text-align:center;">Nosotros tambien somos emprendedores</h3>
+                </div>
+                <p class="mision" style="font-size:25px;text-align:center;">Conocemos y entendemos tus necesidades</p>
+                <p class="mision2" align="center">Sabemos que en ocasiones es algo difícil crear empresa y contar con el apoyo necesario para iniciar el camino
+                  hacia tu sueño empresarial. Por eso nosotros nos convertiremos en tus principales aliados en busca de lograr consolidar tu proyecto en Internet.
+                Tenemos amplia experiencia en Diseño Web Emprendedores, respaldada por conocimientos en creación de empresas y marketing digital
+            especializado en pymes.</p>
+            <p class="mision" style="font-size:20px;text-align:center">¡Deja tu proyecto en las mejores manos!</p>
+            </div>
+            <!-- <div class="team-pic">
+                    <img src="assets/images/servicios_digitales_empresa.png" alt="Infografia servicios para empresa" title="Servicios Digitales para empresa" />
+                </div> -->
+        </div>
+    </div>
+</section>
+<section class="section primary pricing inactive" id="s-pricing">
+    <div class="container">
+        <header class="sep active">
+            <div class="section-title">
+                <h2>Nuestros <i>Planes</i></h2>
+                <h3>Diseño Web a tu medida</h3>
+            </div>
+            <p>En Agencia Web Bogotá te ofrecemos precios especiales y facilidades de pago para el desarrollo de tu proyecto de emprendimiento empresarial</p>
+        </header>
+        <div class="row price-charts">
+          <div class="span-4 price-chart-container">
+              <div class="price-chart">
+                  <div class="ribbon ribbon-large">
+                      <div class="banner">
+                          <div class="text">Mejor Oferta</div>
+                      </div>
+                  </div>
+                  <h4>Sitio Basico</h4>
+                  <h5>El ideal para comenzar</h5>
+                  <div class="price">
+                      <p><span>$79</span>.000 + IVA/mes</p>
+                  </div>
+                  <ul>
+                      <li><span><i class="fa fa-desktop"></i> Diseño Personalizado</span></li>
+                      <li><span><i class="fa fa-search"></i> Posicionamiento SEO</span></li>
+                      <li><span><i class="fa fa-mobile"></i> Versión Móviles</span></li>
+                      <li><span><i class="fa fa-support"></i> Hosting y Dominio Incluído</span></li>
+                  </ul>
+                  <div class="buy-now">
+                      <a href="contacto.php" class="button brand-1 full-width">¡Contrátalo Ahora!</a>
+                  </div>
+              </div>
+          </div>
+            <div class="span-4 price-chart-container">
+                <div class="price-chart">
+                    <h4>Sitio Corporativo</h4>
+                    <h5>Ideal para PYMES</h5>
+                    <div class="price">
+                        <p><small>Desde </small><span>$650</span>.000+IVA</p>
+                    </div>
+                    <ul>
+                        <li><span><i class="fa fa-html5"></i> WordPress o HTML5</span></li>
+                        <li><span><i class="fa fa-envelope"></i> Correos Corporativos</span></li>
+                        <li><span><i class="fa fa-database"></i> Bases de datos</span></li>
+                        <li><span><i class="fa fa-code"></i> Contenido Administrable</span></li>
+                    </ul>
+                    <div class="buy-now">
+                        <a href="contacto.php" class="button brand-1 full-width">¡Contrátalo Ahora!</a>
+                    </div>
+                </div>
+            </div>
+            <div class="span-4 price-chart-container">
+                <div class="price-chart">
+                    <h4>Tienda Online</h4>
+                    <h5>E - Commerce</h5>
+                    <div class="price">
+                        <p><small>Desde </small><span>$900</span>.000+IVA</p>
+                    </div>
+                    <ul>
+                        <li><span><i class="fa fa-wordpress"></i> Woocomerce o Prestashop</span></li>
+                        <li><span><i class="fa fa-shopping-cart"></i> Carrito de Compra</span></li>
+                        <li><span><i class="fa fa-credit-card"></i> Pasarela de Pagos Online</span></li>
+                        <li><span><i class="fa fa-lock"></i> Seguridad SSL</span></li>
+                    </ul>
+                    <div class="buy-now">
+                        <a href="contacto.php" class="button brand-1 full-width">¡Contrátalo Ahora!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="section banner2 alt">
+    <div class="container">
+        <div class="content">
+            <h4>Además <i>Agencia Web Bogotá</i> te cobrará la primer mensualidad cuando tu Sitio Web esté <b>100% Terminado y Online</b> no te exigimos <i>anticipos</i></h4>
+            <p>Contáctanos para más información <a href="contacto.php">sobre nuestros planes y servicios</a></p>
+        </div>
+    </div>
+</section>
+<?php
+    require_once 'includes/footer.php';
+
+ ?>
